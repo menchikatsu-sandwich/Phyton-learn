13f0ffd9125abc20d31d519a4874723da8283af5
menchikatsu-sandwich <santoevo357@gmail.com>
2025-10-30 09:33:29 +0800

    n of oct

--- DIP/coba.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-# Membaca gambar (Pastikan ada file gambar di folder yang sama)
-img = cv2.imread('asszilla.png')
-
-# Menampilkan gambar
-cv2.imshow('My Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
